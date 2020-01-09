e3a103bdc3ebad29b4a603ed1f1eb25f9aad13dc
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-09 05:14:07 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,7 +11,14 @@ class InteractiveRecord
     DB[:conn].results_as_hash = true
 
     sql = "PRAGMA table_info(#{self.table_name})"
+    table_info = DB[:conn].execute(sql)
+    column_names = []
 
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+
+    column_names.compact
   end
 
 end
