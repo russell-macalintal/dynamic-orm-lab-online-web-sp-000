f089300ce855d59ae517687d3d40dd3d87651e23
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-09 05:04:28 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,13 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+
+  end
   
 end
