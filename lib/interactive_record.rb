cfa2865b4d1fad96bae28459bceb80da3733e8b5
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-09 05:51:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,4 +49,13 @@ class InteractiveRecord
     values.join(", ")
   end
 
+  def save
+    sql = <<-SQL 
+      INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+      VALUES (#{values_for_insert})
+      SQL 
+
+    DB[:conn].execute(sql)
+  end
+
 end
